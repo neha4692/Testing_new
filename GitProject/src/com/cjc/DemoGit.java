0acc5203f81f96a468137006ecccb51826223ef4
swapnil <swapnil@192.168.0.105>
2021-12-02 13:10:10 +0530

    by user

--- GitProject/src/com/cjc/DemoGit.java
+++ GitProject/src/com/cjc/DemoGit.java
@@ -1,6 +1,11 @@
 package com.cjc;
 
-public class DemoGit {
+public class DemoGit 
+{
+	public void m1()
+	{
+		System.out.println("this is m1 method");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("hello git");
